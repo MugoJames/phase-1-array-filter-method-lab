730a5506ca8ec4115efdbfc22028185e7272742c
James <jamonjoroge816@gmail.com>
2024-03-25 19:14:27 +0300

    Array filter method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+// FINDING MATCH
+function findMatching(drivers,name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+}
+// FUZZY MATCH
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+// MATCH NAME
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
